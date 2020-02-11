204012a9ae491f8256627a0bf9ae9c0de45972dd
martin <m.scheidt@tu-bs.de>
2020-02-11 12:42:17 +0100

    improved comments in shell script

--- create_ctan-package.sh
+++ create_ctan-package.sh
@@ -3,9 +3,14 @@
 echo "specify version ( e.g. v0.5 ):"
 read VERSION
 
+# create temporary folder
 mkdir tikz-trackschematic-$VERSION
+
+# copy README and .sty-file
 cp README.md tikz-trackschematic-$VERSION/README.md
 cp doc/tikz-trackschematic-documentation.sty  tikz-trackschematic-$VERSION/tikz-trackschematic-documentation.sty
+
+# copy and rename documentation
 cp doc/manual.pdf  tikz-trackschematic-$VERSION/tikz-trackschematic.pdf
 cp doc/manual.tex  tikz-trackschematic-$VERSION/tikz-trackschematic.tex
 cp doc/snippets.pdf  tikz-trackschematic-$VERSION/tikz-trackschematic-snippets.pdf
@@ -14,6 +19,8 @@ mkdir tikz-trackschematic-$VERSION/tikz-trackschematic-examples
 mkdir tikz-trackschematic-$VERSION/tikz-trackschematic-snippets
 cp -R doc/examples/*  tikz-trackschematic-$VERSION/tikz-trackschematic-examples
 cp -R doc/snippets/*  tikz-trackschematic-$VERSION/tikz-trackschematic-snippets
+
+# copy src
 cp src/tikzlibrarytrackschematic.code.tex tikz-trackschematic-$VERSION/tikzlibrarytrackschematic.code.tex
 cp src/tikzlibrarytrackschematic.constructions.code.tex tikz-trackschematic-$VERSION/tikzlibrarytrackschematic.constructions.code.tex
 cp src/tikzlibrarytrackschematic.messures.code.tex tikz-trackschematic-$VERSION/tikzlibrarytrackschematic.messures.code.tex
@@ -21,6 +28,9 @@ cp src/tikzlibrarytrackschematic.topology.code.tex tikz-trackschematic-$VERSION/
 cp src/tikzlibrarytrackschematic.trafficcontrol.code.tex tikz-trackschematic-$VERSION/tikzlibrarytrackschematic.trafficcontrol.code.tex
 cp src/tikzlibrarytrackschematic.vehicles.code.tex tikz-trackschematic-$VERSION/tikzlibrarytrackschematic.vehicles.code.tex
 
+# zip package
 zip -r tikz-trackschematic-$VERSION.zip tikz-trackschematic-$VERSION/*
+
+#cleanup
 rm -rf tikz-trackschematic-$VERSION/*
 rmdir tikz-trackschematic-$VERSION
